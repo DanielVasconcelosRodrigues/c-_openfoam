6acf8b58a5e074e3b6d81d4a5e48d80b0ba1368e
Daniel Rodrigues <daniel.vasconcelos.rd@gmail.com>
2020-06-23 19:31:26 +0100

    more code added

---
+++ basicCode/math.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+int main() {
+	double a,b;
+    cout << "introduce the first value\n";
+	cin >> a;
+    cout << "introduce the second value\n";
+	cin >> b;
+	cout << "a soma é " << a+b << endl;
+	return 0;
+}
