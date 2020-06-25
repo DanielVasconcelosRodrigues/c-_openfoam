105001492e1019de5f4e6c9b55ea9f612e943b51
Daniel Rodrigues <daniel.vasconcelos.rd@gmail.com>
2020-06-25 17:53:49 +0100

    introduction to openFoam

--- openFOAMExample/hello_foam.C
+++ openFOAMExample/hello_foam.C
@@ -4,6 +4,36 @@
 
 int main( int argc, char *argv[] ) 
 {
-	Info << "Hello FOAM !\n";
+	dimensionedScalar inputPressure =
+		dimensionedScalar
+		(
+			"pressure", /// A name field
+			dimensionSet( 1, -1, -2, 0, 0, 0, 0 ),
+			1.013 * 100000 /// Value to initialize
+		);	
+	
+	dimensionedScalar inputVelocity =
+		dimensionedScalar
+		(
+			"velocity", /// A name field
+			dimensionSet( 0, 1, -1, 0, 0, 0, 0 ),
+			4.0 /// Value to initialize
+		);
+		
+	dimensionedScalar Mach =
+		dimensionedScalar
+		(
+			"dimless", /// A name field
+			dimless,
+			3.0 /// Value to initialize
+		);
+	
+	inputPressure.value() = Mach.value();
+		
+	Info << inputPressure << "\n";
+	Info << inputVelocity << "\n";		
+	Info << Mach << "\n";	
 	return 0;
+	
+	
 }
